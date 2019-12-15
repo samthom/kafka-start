f4d9cc5eab93b4769a826f4947150afd4a0b2565
sam338 <sam@three38inc.com>
2019-12-15 16:05:48 +0530

    UPDATE: Implement producer with key and Consumer bug consumer is only reading from partition 2.

---
+++ src/main/java/com/github/samthom/kafka/first/Consumer.java
@@ -0,0 +1,50 @@
+package com.github.samthom.kafka.first;
+
+import kafka.tools.ConsoleConsumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Consumer {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Consumer.class.getName());
+        // create consumer config
+        Properties properties = new Properties();
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "application";
+        String topic = "students";
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        // subscribe consumer to our topic(s)
+        consumer.subscribe(Arrays.asList(topic));
+
+        // poll for new data
+        while(true){
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100)); // new in Kafka 2.0.0
+
+            for (ConsumerRecord<String, String> record : records){
+                logger.info("Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Partition: " + record.partition() + ", Offset:" + record.offset());
+            }
+        }
+    }
+}
---
+++ src/main/java/com/github/samthom/kafka/first/ProducerWithKey.java
@@ -0,0 +1,65 @@
+package com.github.samthom.kafka.first;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerWithKey {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Logger logger = LoggerFactory.getLogger(ProducerWithKey.class);
+        String bootstrapServers = "127.0.0.1:9092";
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // create the producer
+        KafkaProducer<String, String> producer =
+                new KafkaProducer<String, String>(properties);
+        for (int i = 0; i<10; i++) {
+            // create a producer record
+            String topic = "students";
+            String value = "Hello world " + Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
+            final ProducerRecord<String, String> record =
+                    new ProducerRecord<String, String>(topic, key,value);
+            logger.info("Key: " + key);
+            /*
+            * id_0 partition 1
+            * id_1 partition 0
+            * id_2 partition 2
+            * id_3 partition 0
+            * id_4 partition 2
+            * id_5 partition 2
+            * id_6 partition 0
+            * id_7 partition 2
+            * id_8 partition 1
+            * id_9 partition 2
+            */
+            // send data - asynchronous
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // executes every time a record is successfully sent or an exception is thrown
+                    if (e == null) {
+                        // record was sent
+                        logger.info("Received new metadata\n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                    } else {
+                        logger.error("Error while producing: ", e);
+                    }
+                }
+            }).get(); // blocks the send to make it sync - Not recommended
+        }
+        // flush the data
+        producer.flush();
+        // close producer
+        producer.close();
+    }
+}
